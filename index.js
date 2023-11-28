4ee3a5a94f54710b717b4beb5842ae05870279a0
Shazzad-Shuvo <md.rahman.shazzadur@gmail.com>
2023-11-28 17:51:54 +0600

    api to check admin in user created

--- index.js
+++ index.js
@@ -56,21 +56,6 @@ async function run() {
                 next();
             })
         }
-        // const verifyToken = (req, res, next) => {
-        //     // console.log('inside verify tokenN:', req.headers.authorization);
-        //     if (!req.headers.authorization) {
-        //         return res.status(401).send({ message: 'Unauthorized access' });
-        //     }
-        //     const token = req.headers.authorization.split(' ')[1];
-        //     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-        //         if (err) {
-        //             return res.status(401).send({ message: 'Unauthorized access' })
-        //         }
-        //         // console.log('Decoded data with token:', decoded);
-        //         req.decoded = decoded;
-        //         next();
-        //     });
-        // };
 
         // users related api
         app.post('/users', async (req, res) => {
